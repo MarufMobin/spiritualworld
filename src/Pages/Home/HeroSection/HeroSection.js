460511808f347cfbc1ab978199eeaf84ad6ae520
Maruf Mobin <mobinh264@gmail.com>
2022-01-18 23:11:31 +0600

    Two Section done

--- src/Pages/Home/HeroSection/HeroSection.js
+++ src/Pages/Home/HeroSection/HeroSection.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ServiceSection from '../ServiceSection/ServiceSection';
+
 import './HeroSection.css'
 const HeroSection = () => {
     return (
@@ -6,11 +8,12 @@ const HeroSection = () => {
             <div className='hero-section'>
                 <div className='container mx-auto md:p-32 '>
                     <div className=''>
-                        <h1 className='md:leading-tight md:text-7xl md:mb-14 md:font-black text-center text-white '>করার মালিক একমাত্র আল্লাহ আমি তো এক চেষ্টা মাত্র </h1>
+                        <h1 className='md:leading-snug md:text-8xl md:mb-14 md:font-black text-center text-white '>করার মালিক একমাত্র আল্লাহ আমি তো চেষ্টা মাত্র </h1>
                         <h2 className='text-2xl font-bold text-center w-9/12 mx-auto text-white'>আপনাকে কিভাবে সাহায্য করতে পারি ? আপনার যদি কোন ধরনের সমাধানের প্রয়োজন হয়, অনুগ্রহ করে পড়ুন এবং স্ক্রোল করুন | আপনার পথ তারার একটি রোড-ম্যাপ দ্বারা আলোকিত হয় এটাই আমাদের কাম্য | </h2>
                     </div>
                 </div>
             </div>
+            <ServiceSection></ServiceSection>
         </section>
     );
 };
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <section className='footer-section'>
+            <h1>Maruf</h1>
+        </section>
+    );
+};
+
+export default Footer;
