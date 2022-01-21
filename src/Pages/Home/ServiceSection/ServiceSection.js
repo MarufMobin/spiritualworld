79736aaee1f110f973005b6761c77d1444b0d48b
Maruf Mobin <mobinh264@gmail.com>
2022-01-21 23:17:18 +0600

    some thing change

--- src/Pages/Home/ServiceSection/ServiceSection.js
+++ src/Pages/Home/ServiceSection/ServiceSection.js
@@ -4,8 +4,8 @@ import './ServiceSection.css'
 const ServiceSection = () => {
     return (
         <section className='service-section'>
-                <div className='container mx-auto md:shadow-lg md:shadow-indigo-500/40  md:py-96 bg-neutral-50 md:rounded-2xl custom-service-inner-container' >
-                    <p className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque placeat cum tempora eligendi doloremque illo facilis commodi voluptatem provident excepturi ducimus corrupti laudantium, quos aperiam. Suscipit reprehenderit perferendis voluptatibus distinctio.</p>
+                <div className='container mx-auto md:shadow-lg md:shadow-indigo-500/40  md:py-96 service-section-cust md:rounded-2xl custom-service-inner-container' >
+                   
                 </div>
         </section>
     );
