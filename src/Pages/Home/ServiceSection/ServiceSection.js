b1f0ff0d9ba9ba976a27ac391c2fdd44ebf4502d
Maruf Mobin <mobinh264@gmail.com>
2022-01-28 18:17:31 +0600

    card decorated

--- src/Pages/Home/ServiceSection/ServiceSection.js
+++ src/Pages/Home/ServiceSection/ServiceSection.js
@@ -4,8 +4,13 @@ import './ServiceSection.css'
 const ServiceSection = () => {
     return (
         <section className='service-section'>
-                <div className='container mx-auto md:shadow-lg md:shadow-indigo-500/40  md:py-96 service-section-cust md:rounded-2xl custom-service-inner-container' >
-                   
+                <div className='container mx-auto md:shadow-lg md:shadow-indigo-500/40 p-24 service-section-cust md:rounded-2xl custom-service-inner-container' >
+                    <div className="flex flex-wrap justify-center  justify-between">
+                        <div className="one">dsfkljsadfjkljsdfkljasd</div>
+                        <div className="one">adsfasdfsdaf</div>
+                        <div className="one">asdfasdf</div>
+                        <div className="one">asdfasdf</div>
+                    </div>
                 </div>
         </section>
     );
